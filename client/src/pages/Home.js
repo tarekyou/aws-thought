bf86cc3cadbee597e0383950881ce3bedc74f260
tarekyou <tarekyoussef88@hotmail.com>
2022-04-02 16:51:24 -0700

    integrate dynamodb

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import ThoughtList from '../components/ThoughtList';
-import ThoughtForm from '../components/ThoughtForm';
+import React, { useState, useEffect } from "react";
+import ThoughtList from "../components/ThoughtList";
+import ThoughtForm from "../components/ThoughtForm";
 
 const Home = () => {
   const [isLoaded, setIsLoaded] = useState(false);
   const [thoughts, setThoughts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("/api/users");
+        const jsonData = await res.json();
+        const _data = jsonData.sort((a, b) =>
+          a.createdAt < b.createdAt ? 1 : -1
+        );
+        setThoughts([..._data]);
+        setIsLoaded(true);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <main>
@@ -16,8 +33,12 @@ const Home = () => {
           {!isLoaded ? (
             <div>Loading...</div>
           ) : (
-              <ThoughtList thoughts={thoughts} setThoughts={setThoughts} title="Some Feed for Thought(s)..." />
-            )}
+            <ThoughtList
+              thoughts={thoughts}
+              setThoughts={setThoughts}
+              title="Some Feed for Thought(s)..."
+            />
+          )}
         </div>
       </div>
     </main>
